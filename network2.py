a2e80041c5fd862669c6771c9227668f2b9d201a
Your Name <you@example.com>
2021-02-15 18:58:51 +0800

    init

---
+++ network2.py
@@ -0,0 +1,33 @@
+import json
+import random
+import sys
+import numpy as np
+
+class QuadraticCost (object):
+    @staticmethod
+    def fn(a, y):
+        return 0.5* np.linalg.norm(a-y)**2
+    @staticmethod
+    def delta(z, a, y):
+        return (a-y) * sigmoid_prime (z)
+
+class CrossEntropyCost(object):
+    @staticmethod
+    def fn(a, y):
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+    @staticmethod
+    def delta(z, a, y):
+        return (a-y)
+
+class Network(object):
+    def __init__(self, sizes, cost=CrossEntropyCost):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.default_weight_initializer()
+        self.cost = cost
+    def default_weight_initializer(self):
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x)/np.sqrt(x) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+    def large_weight_initializer(self):
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
